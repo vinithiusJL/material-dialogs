e2a19918916eca99b6e77c3a4fdd5b34367b81d3
Marcos Filho <marcos.filho@johnlewis.co.uk>
2022-12-09 15:07:28 +0000

    Update Android X

--- commons/src/main/java/com/afollestad/materialdialogs/prefs/PrefUtil.java
+++ commons/src/main/java/com/afollestad/materialdialogs/prefs/PrefUtil.java
@@ -5,8 +5,8 @@ import android.content.res.TypedArray;
 import android.content.res.XmlResourceParser;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 
 import com.afollestad.materialdialogs.commons.R;
